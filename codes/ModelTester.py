946cec37c9bf11278622c6ad436f406b72758362
Budhaditya Mukhopadhyay <62721668+BudhaTronix@users.noreply.github.com>
2021-03-04 04:48:52 +0100

    Update file names

--- codes/ModelTester.py
+++ codes/ModelTester.py
@@ -6,9 +6,9 @@ import torch.nn
 from tqdm import tqdm
 from Display_Subjects import show_slices
 device = "cuda:6"
-inp_path = "/project/mukhopad/tmp/BlurDetection_tmp/Dataset/ixi_root/T1_mini"
+inp_path = "/project/mukhopad/tmp/BlurDetection_tmp/Dataset/ixi_root/T2_mini"
 _, dataset = datasetLoader(inp_path)
-print('Number of subjects in T1 dataset:', len(dataset))
+print('Number of subjects in T2 dataset:', len(dataset))
 
 #PATH = '../model_weights/BlurDetection_ModelWeights.pth'
 PATH = '../model_weights/BlurDetection_ModelWeights_SinlgeGPU.pth' #model_U_Net.pth'
@@ -17,8 +17,8 @@ net.eval().to(device)
 
 patch_size = 64,128,128
 patch_overlap = 0
-c_sample = dataset[18]
-ic_sample = dataset[32]
+c_sample = dataset[6]
+ic_sample = dataset[12]
 L1 = torch.Tensor(c_sample['label']).long()
 L2 = torch.Tensor(ic_sample['label']).long()
 classes = ('Not Blur','Blur')
@@ -40,7 +40,6 @@ with torch.no_grad():
     for patches_batch in tqdm(patch_loader):
         input_tensor = patches_batch['image'][tio.DATA].float().to(device)
         output = torch.sigmoid(net(input_tensor))
-        print(output)
         tot_out = tot_out + output
         counter +=1
     output_tensor = tot_out/counter
@@ -48,10 +47,13 @@ with torch.no_grad():
     else: output_tensor = 0
     out_1 = output_tensor
 
-    print("Another pic")
+    #print("second IMage")
     #######################################################################
 
-    batch_img = ic_sample
+    batch_img = ic_sample['image'][tio.DATA]
+    batch_img = batch_img.permute(0, 3, 1, 2)
+    batch_img = tio.Subject(image=tio.ScalarImage(tensor=batch_img))
+
     grid_sampler = tio.inference.GridSampler(batch_img, patch_size, patch_overlap,)
     patch_loader = torch.utils.data.DataLoader(grid_sampler,batch_size=1)
     #aggregator = tio.inference.GridAggregator(grid_sampler, overlap_mode="average")
