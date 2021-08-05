b0c8b96c225f26720b8bbe7ccd4027940b3ac79a
Budhaditya <budha2011@gmail.om>
2021-08-05 19:45:09 +0200

    Commit from Office System - SSIM check and creator

---
+++ codes/Utils/DatasetCreator_SSIM.py
@@ -0,0 +1,30 @@
+import skimage.metrics as ski
+import torchio as tio
+from pathlib import Path
+import os
+
+
+path_reg = "/media/hdd_storage/Budha/Dataset/reg_2/"
+path_main = "/media/hdd_storage/Budha/Dataset/Isotropic/"
+regPath = Path(path_reg)
+mainPath = Path(path_main)
+for reg_fileName in regPath.glob("*.nii.gz"):
+    regFilePath = reg_fileName
+    temp = str(reg_fileName.name)
+    si = reg_fileName.name.split(".nii.gz")[0].split("-")[-1]
+    sigma = str("-" + reg_fileName.name.split(".nii.gz")[0].split("-")[-1] + ".nii.gz")
+    reg_fileName = temp.replace(sigma, "")
+    for main_file_name in mainPath.glob("*T1*.nii.gz"):
+        if main_file_name.name == str(reg_fileName + ".nii.gz"):
+            subject_reg = tio.Subject(image=tio.ScalarImage(regFilePath))
+            subject_main = tio.Subject(image=tio.ScalarImage(main_file_name))
+            P = subject_reg["image"][tio.DATA].squeeze(0).float()
+            GT = subject_main["image"][tio.DATA].squeeze(0).float()
+            GT = (GT - GT.min()) / (GT.max() - GT.min())
+            P = (P - P.min()) / (P.max() - P.min())
+            ssim = ski.structural_similarity(GT.numpy(), P.numpy())
+            tempStr = str(reg_fileName + "-" + str(ssim) + ".nii.gz")
+            print(regFilePath.name)
+            print(tempStr)
+
+            os.rename(regFilePath,Path(regFilePath.parent, tempStr))
---
+++ codes/Utils/DatasetCreator_Sigma_Check.py
@@ -0,0 +1,57 @@
+from tqdm import tqdm
+from pathlib import Path
+import torchio as tio
+import time
+import numpy as np
+
+from codes.Utils.motion import MotionCorrupter
+
+output = []
+path = "/media/hdd_storage/Budha/Dataset/a/"
+in_path = "/media/hdd_storage/Budha/Dataset/Isotropic/"
+out_path = "/media/hdd_storage/Budha/Dataset/a/"
+inPath = Path(in_path)
+mainPath = Path(in_path)
+inpPath = Path(path)
+for file_name in sorted(inpPath.glob("*.nii.gz")):
+    temp = str(file_name.name)
+    fileName = temp
+    if fileName not in output:
+        output.append(fileName)
+
+print("Total Files : ", len(output))
+
+output = []
+for file_name in sorted(inpPath.glob("*T1*.nii.gz")):
+    temp = str(file_name.name)
+    sigma = str("-" + file_name.name.split(".nii.gz")[0].split("-")[-1] + ".nii.gz")
+    fileName = temp.replace(sigma, "")
+    if fileName not in output:
+        output.append(fileName)
+
+print("Total Subjects : ", len(output))
+#####################################################################################################################################
+
+for subject in output:
+    c = 0
+    for file_name in sorted(inpPath.glob("*.nii.gz")):
+        temp = str(file_name.name)
+        sigma = str("-" + file_name.name.split(".nii.gz")[0].split("-")[-1] + ".nii.gz")
+        fileName = temp.replace(sigma, "")
+        if fileName == subject:
+            c = c + 1
+    print(subject, " - ",c)
+    if c<=10:
+        no_of_corr = 10 - c
+        for file_name in sorted(mainPath.glob("*T1*.nii.gz")):
+            if file_name.name == str(subject)+".nii.gz":
+                for i in range(no_of_corr):
+                    print("Starting to corrupt ", file_name.name, "    Iteration Number - ", i, "/", no_of_corr)
+                    subject = tio.Subject(image=tio.ScalarImage(file_name))
+                    sigma = np.random.uniform(low=0.01, high=0.2, size=(1,))
+                    moco = MotionCorrupter(mode=2, n_threads=5, mu=0.0, sigma=sigma, random_sigma=False)
+                    transforms = [tio.Lambda(moco.perform, p=1)]
+                    transform = tio.Compose(transforms)
+                    img = transform(subject["image"][tio.DATA])[1:2, :, :, :]
+                    temp = tio.ScalarImage(tensor=img)
+                    temp.save(out_path + str(file_name.name.split(".nii.gz")[0]) + "-" + str(sigma[0]) + '.nii.gz', squeeze=True)
